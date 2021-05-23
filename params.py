a474a84b91464ba6fdf019ed9e1e272acee9ae91
Alexis Anzaldo <79902516+alexisanzaldo@users.noreply.github.com>
2021-05-22 21:38:10 -0700

    Add files via upload

--- params.py
+++ params.py
@@ -1,15 +1,12 @@
 # Scenario layout Parameters ------------------------------------------------------------------------------------------
-import numpy as np
 instances= 10#10
-#Nbs = np.random.randint(4,19)  # 7#3#19
-#Nue = np.copy(Nbs)  # 7#3#19
 Nbs = 19
 Nue = 19
 #this parameters are needed only on poisson deployment -----------------------------------------------------------------
 width = 180
 height = 180
-#cell_position='grid' # ir cell_position='grid'  OR 'poisson'
-cell_position = 'poisson'
+cell_position='grid' # ir cell_position='grid'  OR 'poisson'
+#cell_position = 'poisson'
 # Scenario layout Parameters ------------------------------------------------------------------------------------------
 
 # Neural Network Parameters -------------------------------------------------------------------------------------------
@@ -22,15 +19,11 @@ warm_ep = 100
 num_games = 5000 + warm_ep  # Episodes
 exp_games = 3000 + warm_ep
 
-
-#warm_ep = 10
-#num_games = 200 + warm_ep  # Episodes
-#exp_games = 100 + warm_ep
-
+#warm_ep = 100
+#num_games = 500 + warm_ep  # Episodes
+#exp_games = 300 + warm_ep
 
 
-
-# ---------------------------------------------------------------------------------------------------------------------
 
 # Validation parameters -----------------------------------------------------------------------------------------------
 Validation_Scenarios = 15                               # Number of fixed scenarios
@@ -39,7 +32,6 @@ AGGREGATE_STATS_EVERY = 50  # episodes
 stat_index = int(AGGREGATE_STATS_EVERY/Validate_Freq)   # value for graphs
 validation_steps = 5                                    # Number of intervals evaluated on each validation episode
 # Validation parameters -----------------------------------------------------------------------------------------------
-
 # Network Parameters --------------------------------------------------------------------------------------------------
 Pmax_dBm =23 # 23  # Para este ejercicio (1)
 Rcell = 25  # 30
